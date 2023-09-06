8cdf04de854ec8701e4d5e5cd28541c91dea78f1
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-09-06 14:10:48 +0530

    popup dialog for logout confirmation

--- mobile-app/visitlog/lib/Components/drawer.dart
+++ mobile-app/visitlog/lib/Components/drawer.dart
@@ -58,7 +58,7 @@ class DrawerWidget extends StatelessWidget {
                   height: 50,
                 )
               ],
-            )
+            ),
           ],
         ),
       ),
@@ -95,11 +95,61 @@ class DrawerWidget extends StatelessWidget {
             context, MaterialPageRoute(builder: (context) => Profile()));
         break;
       case 3:
-        await AuthService().signOutGoogle();
-        // ignore: use_build_context_synchronously
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => LoginScreen()));
+        _showDialog(context);
         break;
     }
+  }
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          // title: Text("Logout"),
+          content: Container(
+            height: 86,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/icon/7090891.png',
+                  height: 45,
+                ),
+                const Text(
+                  'Are you sure ?',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 167, 84, 78),
+                  ),
+                ),
+                const Text(
+                  'Do you want to logout',
+                  style: TextStyle(color: Colors.black87),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10, bottom: 10),
+              child: TextButton(
+                onPressed: () async {
+                  await AuthService().signOutGoogle();
+                  // ignore: use_build_context_synchronously
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                child: const Text('Yes'),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- mobile-app/visitlog/lib/Components/profile_button.dart
+++ mobile-app/visitlog/lib/Components/profile_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:visitlog/Screens/login_screen.dart';
 import 'package:visitlog/services/auth_service.dart';
 
 // ignore: must_be_immutable
@@ -34,24 +35,29 @@ class _ProfileButtonState extends State<ProfileButton> {
           height:
               _expanded ? 50 : 50, // Adjust height based on the expanded state
           child: GestureDetector(
-            onTap: () {
-              setState(() {
-                _expanded = !_expanded; // Toggle the expanded state
-              });
+            onDoubleTap: () {
+              _showDialog();
             },
             child: Row(
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: _expanded
                   ? [
-                      CircleAvatar(
-                        backgroundImage: Image.network(
-                          userImage ?? '',
-                          height: 40,
-                          width: 40,
-                        ).image,
-                        maxRadius: 20,
-                        minRadius: 15,
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _expanded = !_expanded; // Toggle the expanded state
+                          });
+                        },
+                        child: CircleAvatar(
+                          backgroundImage: Image.network(
+                            userImage ?? '',
+                            height: 40,
+                            width: 40,
+                          ).image,
+                          maxRadius: 20,
+                          minRadius: 15,
+                        ),
                       ),
                       const SizedBox(width: 12),
                       AnimatedOpacity(
@@ -59,48 +65,60 @@ class _ProfileButtonState extends State<ProfileButton> {
                         opacity: _expanded
                             ? 1.0
                             : 0.0, // Control opacity based on expanded state
-                        child: FutureBuilder(
-                          future: Future.delayed(
-                            const Duration(
-                                milliseconds:
-                                    380), // Delay the appearance of the Column
-                            () => true,
-                          ),
-                          builder: (context, snapshot) {
-                            if (snapshot.connectionState ==
-                                    ConnectionState.waiting ||
-                                !snapshot.hasData) {
-                              return Container(); // Return an empty container while waiting
-                            }
-                            return Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  UserName ?? '',
-                                  style: const TextStyle(
-                                      fontSize: 14, color: Colors.black87),
-                                ),
-                                Text(
-                                  UserEmail ?? '',
-                                  style: const TextStyle(
-                                      fontSize: 10, color: Colors.black54),
-                                ),
-                              ],
-                            );
+                        child: GestureDetector(
+                          onTap: () {
+                            _showDialog();
                           },
+                          child: FutureBuilder(
+                            future: Future.delayed(
+                              const Duration(
+                                  milliseconds:
+                                      380), // Delay the appearance of the Column
+                              () => true,
+                            ),
+                            builder: (context, snapshot) {
+                              if (snapshot.connectionState ==
+                                      ConnectionState.waiting ||
+                                  !snapshot.hasData) {
+                                return Container(); // Return an empty container while waiting
+                              }
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    UserName ?? '',
+                                    style: const TextStyle(
+                                        fontSize: 14, color: Colors.black87),
+                                  ),
+                                  Text(
+                                    UserEmail ?? '',
+                                    style: const TextStyle(
+                                        fontSize: 10, color: Colors.black54),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
                         ),
                       ),
                     ]
                   : [
-                      CircleAvatar(
-                        backgroundImage: Image.network(
-                          userImage ?? '',
-                          height: 50,
-                          width: 50,
-                        ).image,
-                        maxRadius: 25,
-                        minRadius: 20,
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _expanded = !_expanded; // Toggle the expanded state
+                          });
+                        },
+                        child: CircleAvatar(
+                          backgroundImage: Image.network(
+                            userImage ?? '',
+                            height: 50,
+                            width: 50,
+                          ).image,
+                          maxRadius: 25,
+                          minRadius: 20,
+                        ),
                       ),
                     ],
             ),
@@ -108,6 +126,59 @@ class _ProfileButtonState extends State<ProfileButton> {
         ),
         SizedBox(width: 6.0.w),
       ],
+    );
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          // title: Text("Logout"),
+          content: Container(
+            height: 86,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/icon/7090891.png',
+                  height: 45,
+                ),
+                const Text(
+                  'Are you sure ?',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: Color.fromARGB(255, 167, 84, 78),
+                  ),
+                ),
+                const Text(
+                  'Do you want to logout',
+                  style: TextStyle(color: Colors.black87),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10, bottom: 10),
+              child: TextButton(
+                onPressed: () async {
+                  await AuthService().signOutGoogle();
+                  // ignore: use_build_context_synchronously
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                },
+                child: const Text('Yes'),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
