9d7a59935af21fd8f4b4a346569b09d1e719b2a5
G5andeepD <e19063@eng.pdn.ac.lk>
2023-09-07 11:57:40 +0530

    firestore config

--- ADMIN_DASHBOARD/src/Components/NavigationTemplate/Navigation.jsx
+++ ADMIN_DASHBOARD/src/Components/NavigationTemplate/Navigation.jsx
@@ -2,10 +2,16 @@ import { useEffect } from "react";
 import Nav from "../NaviconTemplate/Nav";
 import { auth } from "../../config/firebase";
 import "./Navigation.css";
+import { useNavigate } from "react-router-dom";
 
 export const Navigation = () => {
+  const navigate = useNavigate();
+  const handleItemClick = (path) => {
+    navigate(path);
+  };
+
   useEffect(() => {
-    console.log(auth?.currentUser?.displayName ||auth?.currentUser?.email);
+    console.log(auth?.currentUser?.displayName || auth?.currentUser?.email);
     console.log(auth?.currentUser?.photoURL || "No Image");
   }, []);
   return (
@@ -14,11 +20,42 @@ export const Navigation = () => {
         <div className="profile">
           <img src="/logo.jpg" alt="user-img" className="profile-img" />
         </div>
+        
         <span></span>
       </header>
+      <ul >
+          <li
+            
+            onClick={() => handleItemClick("/dashboard")}
+          >
+            Dashboard<span></span>
+          </li>
+          <li
+            
+            onClick={() => handleItemClick("/clients")}
+          >
+            Clients<span></span>
+          </li>
+          <li
+            
+            onClick={() => handleItemClick("/tech")}
+          >
+            Technicians<span></span>
+          </li>
+          <li
+            
+            onClick={() => handleItemClick("/tasks")}
+          >
+            Tasks<span></span>
+          </li>
+          <li
+            
+            onClick={() => handleItemClick("/techassign")}
+          >
+           Technician Assign<span></span>
+          </li>
+        </ul>
       <Nav />
     </div>
   );
 };
-
-
