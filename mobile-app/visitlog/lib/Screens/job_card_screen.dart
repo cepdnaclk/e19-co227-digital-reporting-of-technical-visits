36960ef63e8d69c280a3707f3d33f556467bfa98
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-09-13 23:43:12 +0530

    Job Card Screen

--- mobile-app/visitlog/lib/Screens/job_card_screen.dart
+++ mobile-app/visitlog/lib/Screens/job_card_screen.dart
@@ -1,16 +1,19 @@
-
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:visitlog/Components/drawer.dart';
 import 'package:visitlog/Components/upper_bar.dart';
-
+import 'package:visitlog/Widgets/job_list.dart';
 import '../Widgets/bottom_navigation.dart';
+import 'package:visitlog/Data/tasks.dart';
 
+// ignore: must_be_immutable
 class JobCard extends StatelessWidget {
   JobCard({super.key});
   static String id = "job_card_page";
+  late String keyword;
+  final List<Map<String, String>> items = TaskList().items;
 
- final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +21,10 @@ class JobCard extends StatelessWidget {
       home: Scaffold(
         key: _globalKey,
         drawer: DrawerWidget(id: id),
-        bottomNavigationBar: NavBar(id: id, indexNum: 1,),
+        bottomNavigationBar: NavBar(
+          id: id,
+          indexNum: 1,
+        ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
@@ -42,6 +48,88 @@ class JobCard extends StatelessWidget {
               thickness: 3,
               indent: 8.0.w,
               endIndent: 8.0.w,
+            ),
+            SizedBox(
+              height: 3.0.h,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 10.0.w,
+                ),
+                const Text(
+                  "Sort",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                SizedBox(
+                  width: 1.0.w,
+                ),
+                Icon(Icons.arrow_drop_down_outlined),
+                const Spacer(),
+                Form(
+                  child: Container(
+                    width: 51.0.w,
+                    height: 10.0.w,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: TextFormField(
+                      style: TextStyle(color: Colors.black45),
+                      onChanged: (value) {
+                        keyword = value;
+                      },
+                      decoration: InputDecoration(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(18),
+                                  borderSide: const BorderSide(
+                                    color: Color.fromARGB(255, 221, 232, 250),
+                                  )),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(18),
+                                  borderSide: BorderSide(
+                                    color: Color.fromARGB(255, 213, 226, 247),
+                                  )
+                        ),          
+                        fillColor: Color.fromARGB(255, 221, 232, 250),
+                        suffixIcon: const Padding(
+                          padding: EdgeInsetsDirectional.only(start: 16, end: 16),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.black38,
+                          ),
+                        ),
+                        hintText: 'Search......',
+                        hintStyle: TextStyle(color: Colors.black38),
+                        
+                      ),
+                      textAlignVertical: TextAlignVertical.center,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 10.0.w,)
+              ],
+            ),
+            Expanded(
+              child: 
+                  ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return BuildItem(
+                        index: index,
+                      );
+                    },
+                  ),
+                  
+              ),
+            SizedBox(
+              height: 6.0.h,
             )
           ],
         ),
---
+++ mobile-app/visitlog/lib/Widgets/job_list.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:visitlog/Data/tasks.dart';
+
+class BuildItem extends StatelessWidget {
+  BuildItem({super.key, required this.index});
+  final List<Map<String, String>> items = TaskList().items;
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, right: 30, bottom: 10),
+      child: SizedBox(
+        height: 75,
+        child: Card(
+          color: Color.fromARGB(255, 55, 55, 55),
+          shadowColor: const Color.fromARGB(220, 50, 152, 192),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          elevation: 10, // Adjust elevation as needed
+          margin: const EdgeInsets.all(4.0), // Add margin for spacing
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8),
+            child: ListTile(
+              leading: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.work, color: Colors.white),
+                ],
+              ),
+              title: Text(
+                items[index]['name'] ?? '',
+                style: const TextStyle(color: Colors.white),
+              ),
+              subtitle: Text(
+                items[index]['subTopic'] ?? '',
+                style:
+                    const TextStyle(color: Color.fromARGB(255, 185, 227, 247)),
+              ),
+              trailing: IconButton(
+                icon: const Icon(
+                  Icons.arrow_forward,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  _showDescriptionDialog(
+                    context,
+                    items[index]['description'] ?? '',
+                    items[index]['name'] ?? '',
+                    items[index]['subTopic'] ?? '',
+                    items[index]['location'] ?? '',
+                    items[index]['time'] ?? '',
+                  );
+                },
+              ),
+              onTap: () {
+                _showDescriptionDialog(
+                  context,
+                  items[index]['description'] ?? '',
+                  items[index]['name'] ?? '',
+                  items[index]['subTopic'] ?? '',
+                  items[index]['location'] ?? '',
+                  items[index]['time'] ?? '',
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDescriptionDialog(BuildContext context, String description,
+      String topic, String subTopic, String location, String time) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(0),
+            child: Stack(children: [
+              // Container(
+              //   height: 200,
+              //   child: _TopPortion(topic: topic, subTopic: subTopic),
+              // ),
+              Container(
+                height: 280,
+                child: Column(
+                  children: [
+                    SizedBox(
+                        height: 92,
+                        child: _TopPortion(topic: topic, subTopic: subTopic)),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: ListTile(
+                        leading: const Icon(Icons.location_city),
+                        title: Text(
+                          topic,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: ListTile(
+                        leading: const Icon(Icons.location_on),
+                        title: Text(
+                          location,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20.0),
+                      child: ListTile(
+                        leading: const Icon(Icons.lock_clock),
+                        title: Text(
+                          time,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ]),
+          ),
+        );
+      },
+    );
+  }
+}
+
+// ignore: unused_element
+class _TopPortion extends StatelessWidget {
+  const _TopPortion({required this.topic, required this.subTopic});
+
+  final String topic;
+  final String subTopic;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Color.fromARGB(255, 12, 71, 51),
+                    Color.fromARGB(195, 12, 71, 32)
+                  ]),
+              borderRadius: BorderRadius.only(
+                // bottomLeft: Radius.circular(50),
+                // bottomRight: Radius.circular(50),
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              )),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: ListTile(
+              leading: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.work, color: Colors.white),
+                ],
+              ),
+              title: Text(
+                subTopic,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
