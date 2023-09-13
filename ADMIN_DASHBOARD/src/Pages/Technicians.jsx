d0b21caa246f0d37c72ab07541e0652ff26aa2bb
G5andeepD <e19063@eng.pdn.ac.lk>
2023-09-13 16:07:14 +0530

    missed issue

--- ADMIN_DASHBOARD/src/Pages/Technicians.jsx
+++ ADMIN_DASHBOARD/src/Pages/Technicians.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+
 import { db } from "../config/firebase";
 import { useState, useEffect } from "react";
 import { collection, doc, getDoc, getDocs ,onSnapshot} from "firebase/firestore";
