6cf1b2af727e424a4aaac70c6764f4c237a7b8e3
G5andeepD <e19063@eng.pdn.ac.lk>
2023-09-15 12:05:14 +0530

    Technician Edit Form

---
+++ ADMIN_DASHBOARD/src/Components/Technicians/TechnicianEditForm.jsx
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from "react";
+import { collection, doc, updateDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import "../../Styles/Technicians/TechnicianForm.scss";
+import {
+  BsFillFileEarmarkPersonFill,
+  BsFileEarmarkPerson,
+  BsTelephoneFill,
+} from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
+
+export const TechnicianEditForm = ({ technician, onClosing }) => {
+  const technicianCollectionRef = collection(db, "Technicians");
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    mobile: [""],
+  });
+
+  useEffect(() => {
+    // Populate the form with the technician's data when it's available
+    if (technician) {
+      setFormData({
+        firstName: technician.firstName || "",
+        lastName: technician.lastName || "",
+        email: technician.email || "",
+        address: technician.address || "",
+        mobile: technician.mobile || [""],
+      });
+    }
+    console.log(formData.mobile)
+  }, [technician]);
+
+  const handleChange = (e, index) => {
+    const { name, value } = e.target;
+    if (name === "mobile") {
+      const updatedMobile = [...formData.mobile];
+      updatedMobile[index] = value;
+
+      setFormData({
+        ...formData,
+        mobile: updatedMobile,
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+  const handleAddMobile = () => {
+    setFormData({
+      ...formData,
+      mobile: [...formData.mobile, ""],
+    });
+  };
+
+  const handleRemoveMobile = (index) => {
+    const updatedMobile = [...formData.mobile];
+    updatedMobile.splice(index, 1);
+
+    setFormData({
+      ...formData,
+      mobile: updatedMobile,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Update the technician's data in the database
+    await onUpdate(formData);
+    onClosing();
+  };
+
+  const onUpdate = async (data) => {
+    console.log(data);
+    // Update the document in Firestore
+    await updateDoc(doc(technicianCollectionRef, technician.id), data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="card">
+      <h2 className="topic">Edit Technician</h2>
+      <button className="close-button" onClick={() => onClosing()}>
+        X
+      </button>
+      <div className="first-name">
+        <div className="icon">
+          <BsFillFileEarmarkPersonFill />
+        </div>
+        <div className="first-name-input">
+          <label htmlFor="firstName">First Name </label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={formData.firstName}
+            onChange={(e) => handleChange(e, 0)}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="last-name">
+        <div className="icon">
+          <BsFileEarmarkPerson />
+        </div>
+        <div className="first-name-input">
+          <label htmlFor="lastName">Last Name </label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={formData.lastName}
+            onChange={(e) => handleChange(e, 1)}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="e-mail">
+        <div className="icon">
+          <AiOutlineMail />
+        </div>
+        <div className="email-input">
+          <label htmlFor="email">Email&emsp;&emsp;&ensp;</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={(e) => handleChange(e, 2)}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="address">
+        <div className="icon">
+          <GoLocation />
+        </div>
+        <div>
+          <label htmlFor="address">Address&emsp;&ensp;</label>
+          <input
+            type="text"
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={(e) => handleChange(e, 3)}
+            required
+          />
+        </div>
+      </div>
+
+      <div className="phone-number">
+        <div className="icon">
+          <BsTelephoneFill />
+        </div>
+        <div className="mobile-number">
+          <label htmlFor="mobile">Mobile Numbers:</label>
+          {formData.mobile.map((mobileNumber, index) => (
+            <div key={index}>
+              <input
+                type="text"
+                name="mobile"
+                value={mobileNumber}
+                onChange={(e) => handleChange(e, index)}
+                required
+              />
+              {index > 0 && (
+                <button type="button" onClick={() => handleRemoveMobile(index)}>
+                  Remove
+                </button>
+              )}
+            </div>
+          ))}
+          <button type="button" onClick={handleAddMobile}>
+            Add Mobile Number
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <button type="submit">Update</button>
+      </div>
+    </form>
+  );
+};
--- ADMIN_DASHBOARD/src/Components/Technicians/TechniciansTable.jsx
+++ ADMIN_DASHBOARD/src/Components/Technicians/TechniciansTable.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import "../../Styles/Technicians/TechniciansTable.scss"; // Import your SCSS stylesheet
 import { BsSortAlphaDown,  BsSortAlphaDownAlt} from "react-icons/bs";
+import { TechnicianEditForm } from "./TechnicianEditForm"; // Import the TechnicianEditForm component
 
-
-export const TechniciansTable = ({ technicians,searchTerm,searchColumn}) => {
-
+export const TechniciansTable = ({ technicians, searchTerm, searchColumn }) => {
   const [sortBy, setSortBy] = useState("firstName");
   const [sortDirection, setSortDirection] = useState("asc");
-  //   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedTechnician, setSelectedTechnician] = useState(null); // State to store the selected technician
+  const [isEditFormVisible, setIsEditFormVisible] = useState(false); // State to control the visibility of the edit form
 
   const handleSort = (field) => {
     if (field === sortBy) {
@@ -30,31 +30,35 @@ export const TechniciansTable = ({ technicians,searchTerm,searchColumn}) => {
   });
 
   const filteredTechnicians = sortedTechnicians.filter((technician) => {
-    if (searchColumn ==="Name"){
+    if (searchColumn === "Name") {
       const fullName =
-      `${technician.firstName} ${technician.lastName}`.toLowerCase();
-    return fullName.includes(searchTerm.toLowerCase());
+        `${technician.firstName} ${technician.lastName}`.toLowerCase();
+      return fullName.includes(searchTerm.toLowerCase());
+    } else if (searchColumn === "Email") {
+      return technician.email.includes(searchTerm.toLowerCase());
+    } else if (searchColumn === "Address") {
+      return technician.address.includes(searchTerm.toLowerCase());
+    } else if (searchColumn === "TP") {
+      return technician.mobile[0].includes(searchTerm.toLowerCase());
     }
-    else if (searchColumn ==="Email"){
-      
-    return technician.email.includes(searchTerm.toLowerCase());
-    }
-    else if (searchColumn ==="Address"){
-    return technician.address.includes(searchTerm.toLowerCase());
-    }
-   
-    else if (searchColumn ==="TP"){
-    return technician.mobile[0].includes(searchTerm.toLowerCase());
-    }
-   
-
-    
   });
+
+  // Function to open the edit form with the selected technician data
+  const openEditForm = (technician) => {
+    setSelectedTechnician(technician);
+    setIsEditFormVisible(true);
+  };
+
+  // Function to close the edit form
+  const closeEditForm = () => {
+    setSelectedTechnician(null);
+    setIsEditFormVisible(false);
+  };
 
   return (
     <div className="table-container-2">
       <table className="tech-table">
-        <thead>
+      <thead>
           <tr>
             <th>
               Technician Name{" "}
@@ -79,10 +83,29 @@ export const TechniciansTable = ({ technicians,searchTerm,searchColumn}) => {
               <td>{technician.email}</td>
               <td>{technician.address}</td>
               <td>{technician.mobile[0]}</td>
+              <td>
+                <button onClick={() => openEditForm(technician)}>Edit</button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      {/* Display the edit form as a modal/popup when needed */}
+      {isEditFormVisible && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeEditForm}>
+              &times;
+            </span>
+            {/* Pass the selected technician to the edit form */}
+            <TechnicianEditForm
+              technician={selectedTechnician}
+              onClosing={closeEditForm}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
