fc0fd26a790318e2d31aee877129e56ad348e35a
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2023-09-15 14:21:31 +0530

    form

--- ADMIN_DASHBOARD/src/Components/Technicians/TechniciansTable.jsx
+++ ADMIN_DASHBOARD/src/Components/Technicians/TechniciansTable.jsx
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
-
-import "../../Styles/Technicians/TechniciansTable.scss"; // Import your SCSS stylesheet
+import "../../Styles/Technicians/TechniciansTable.scss"; 
 import { BsSortAlphaDown, BsSortAlphaDownAlt } from "react-icons/bs";
-import { TechnicianEditForm } from "./TechnicianEditForm"; // Import the TechnicianEditForm component
+import { FaUser, FaEnvelope, FaMapMarker, FaPhone } from "react-icons/fa";
 
 export const TechniciansTable = ({ technicians, searchTerm, searchColumn }) => {
   const [sortBy, setSortBy] = useState("firstName");
   const [sortDirection, setSortDirection] = useState("asc");
-
-  const [selectedTechnician, setSelectedTechnician] = useState(null); // State to store the selected technician
-  const [isEditFormVisible, setIsEditFormVisible] = useState(false); // State to control the visibility of the edit form
-
   const [showTechnicianDetails, setShowTechnicianDetails] = useState(false);
+  const [selectedTechnician, setSelectedTechnician] = useState(null);
 
   const handleSort = (field) => {
     setShowTechnicianDetails(false);
@@ -48,8 +44,7 @@ export const TechniciansTable = ({ technicians, searchTerm, searchColumn }) => {
 
   const filteredTechnicians = sortedTechnicians.filter((technician) => {
     if (searchColumn === "Name") {
-      const fullName =
-        `${technician.firstName} ${technician.lastName}`.toLowerCase();
+      const fullName = `${technician.firstName} ${technician.lastName}`.toLowerCase();
       return fullName.includes(searchTerm.toLowerCase());
     } else if (searchColumn === "Email") {
       return technician.email.includes(searchTerm.toLowerCase());
@@ -59,18 +54,6 @@ export const TechniciansTable = ({ technicians, searchTerm, searchColumn }) => {
       return technician.mobile[0].includes(searchTerm.toLowerCase());
     }
   });
-
-  // Function to open the edit form with the selected technician data
-  const openEditForm = (technician) => {
-    setSelectedTechnician(technician);
-    setIsEditFormVisible(true);
-  };
-
-  // Function to close the edit form
-  const closeEditForm = () => {
-    setSelectedTechnician(null);
-    setIsEditFormVisible(false);
-  };
 
   return (
     <div className="table-container-2">
@@ -121,45 +104,32 @@ export const TechniciansTable = ({ technicians, searchTerm, searchColumn }) => {
               <td>{technician.email}</td>
               <td>{technician.address}</td>
               <td>{technician.mobile[0]}</td>
-              <td>
-                <button onClick={() => openEditForm(technician)}>Edit</button>
-              </td>
             </tr>
           ))}
         </tbody>
       </table>
-
-      {/* Display the edit form as a modal/popup when needed */}
-      {isEditFormVisible && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeEditForm}>
-              &times;
-            </span>
-            {/* Pass the selected technician to the edit form */}
-            <TechnicianEditForm
-              technician={selectedTechnician}
-              onClosing={closeEditForm}
-            />
-          </div>
-        </div>
-      )}
       {showTechnicianDetails && selectedTechnician && (
-        <div
-          className={`technician-details ${
-            showTechnicianDetails ? "" : "closed"
-          }`}
-        >
+        <div className={`technician-details ${showTechnicianDetails ? "" : "closed"}`}>
           <button className="close-button" onClick={closeTechnicianDetails}>
             &#10006;
           </button>
           <h2>Technician Details</h2>
           <p>
+            <FaUser />
             Name: {selectedTechnician.firstName} {selectedTechnician.lastName}
           </p>
-          <p>Email: {selectedTechnician.email}</p>
-          <p>Address: {selectedTechnician.address}</p>
-          <p>Telephone Number: {selectedTechnician.mobile[0]}</p>
+          <p>
+            <FaEnvelope />
+            Email: {selectedTechnician.email}
+          </p>
+          <p>
+            <FaMapMarker />
+            Address: {selectedTechnician.address}
+          </p>
+          <p>
+            <FaPhone />
+            Telephone Number: {selectedTechnician.mobile[0]}
+          </p>
         </div>
       )}
     </div>
