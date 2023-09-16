f170c054e90e66842f94f490edf26ee718ad626f
bimbara28 <115542059+Bimbara28@users.noreply.github.com>
2023-09-16 14:46:23 +0530

    Tasks page

--- ADMIN_DASHBOARD/src/Pages/Tasks.jsx
+++ ADMIN_DASHBOARD/src/Pages/Tasks.jsx
@@ -1,27 +1,25 @@
-import { Navigation } from "../Components/Navigation/Navigation";
-import UserCard from "../Components/UserCard";
+import React, { useState, useEffect } from "react";
 import { collection, onSnapshot, getDoc } from "firebase/firestore";
 import { db } from "../config/firebase";
-import { useState, useEffect } from "react";
-import styles from "../Styles/Tasks.module.scss";
+import { Navigation } from "../Components/Navigation/Navigation";
+import UserCard from "../Components/UserCard";
 import { TasksTable } from "../Components/Tasks/TasksTable";
 import { TaskForm } from "../Components/Tasks/TaskForm";
+import styles from "../Styles/Tasks.module.scss";
 
 export const Tasks = () => {
   const [jobs, setJobs] = useState([]);
-
   const [searchTerm, setSearchTerm] = useState("");
   const [searchColumn, setSearchColumn] = useState("Task Name");
 
+  const [showForm, setShowForm] = useState(false);
+
   useEffect(() => {
-    // Create a reference to the "Jobs" collection in Firestore
     const jobsCollectionRef = collection(db, "Jobs");
 
-    // Set up a Firestore listener for real-time updates (or use getDocs for one-time fetch)
     const unsubscribe = onSnapshot(jobsCollectionRef, async (snapshot) => {
       const updatedJobs = [];
 
-      // Iterate over the jobs
       for (const docRef of snapshot.docs) {
         const jobData = docRef.data();
         var jobWithTechnician = jobData;
@@ -30,12 +28,10 @@ export const Tasks = () => {
 
           const technicianDoc = await getDoc(technicianRef);
           if (technicianDoc.exists()) {
-            // Extract the technician's name
             const technicianName =
               technicianDoc.data().firstName +
               " " +
               technicianDoc.data().lastName;
-            // Combine job data with technician name
             jobWithTechnician = {
               ...jobWithTechnician,
               technicianName,
@@ -50,10 +46,8 @@ export const Tasks = () => {
           const companyName = companyDoc.data().companyName;
           const companyAddress = companyDoc.data().address;
 
-          // Combine job data with technician name
           jobWithTechnician = {
             ...jobWithTechnician,
-
             companyName,
             companyAddress,
           };
@@ -65,21 +59,31 @@ export const Tasks = () => {
       setJobs(updatedJobs);
     });
 
-    // Clean up the listener when the component unmounts
     return () => {
       unsubscribe();
     };
   }, []);
+
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
+
   return (
     <div>
       <div className={styles.container}>
         <Navigation />
         <UserCard />
         <div className={styles.component_container}>
-          <TaskForm />
           <div className={styles.name}>
             <p>Tasks Log</p>
           </div>
+          <button
+            className={styles.create_task_button}
+            onClick={toggleForm}
+          >
+            Create Task
+          </button>
+          {showForm && <TaskForm />}
           <div className={styles.table_container}>
             <div className={styles.search_bar}>
               <input
@@ -89,7 +93,7 @@ export const Tasks = () => {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <p className={styles.lable}>Sort by:</p>
+              <p className={styles.label}>Sort by:</p>
               <select
                 className={styles.search_column_select}
                 value={searchColumn}
