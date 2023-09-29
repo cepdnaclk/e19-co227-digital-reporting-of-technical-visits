3f5a1aad23ecddfdcba72be392ea94271c978b6c
Chandula Adhikari <e19008@eng.pdn.ac.lk>
2023-09-30 00:54:53 +0530

    made logout functionalities

--- ADMIN_DASHBOARD/src/Components/UserCard.jsx
+++ ADMIN_DASHBOARD/src/Components/UserCard.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styles from "../Styles/UserCard.module.scss";
 import { auth } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
 
 const UserCard = () => {
   const [user, setUser] = useState(null);
+  const [showLogoutButton, setShowLogoutButton] = useState(false);
+  const [showLogoutDialog, setShowLogoutDialog] = useState(false); // New state variable
   const navigate = useNavigate();
+
+  const logoutButtonRef = useRef(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
@@ -17,24 +21,79 @@ const UserCard = () => {
       }
     });
 
-    return () => unsubscribe();
+    // Cleanup code for the auth state listener
+    return () => {
+      unsubscribe();
+    };
   }, [navigate]);
 
-  return (
-    <div className={styles.user_card}>
-      {user ? (
-        <div className={styles.user_info}>
-          <img src={user.photoURL} alt="User" />
-          <div className={styles.user_details}>
-            <p>{user.displayName}</p>
-            <p>{user.email}</p>
-          </div>
+  const handleLogout = () => {
+    // Show the logout confirmation dialog
+    setShowLogoutDialog(true);
+  };
+
+  const confirmLogout = () => {
+    // Perform the logout action
+    auth
+      .signOut()
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Error signing out:", error);
+      });
+
+    // Hide the logout confirmation dialog
+    setShowLogoutDialog(false);
+  };
+
+  const toggleLogoutButton = () => {
+    setShowLogoutButton(!showLogoutButton);
+  };
+
+  const cancelLogout = () => {
+    // Hide the logout confirmation dialog
+    setShowLogoutDialog(false);
+  };
+
+  return (<>
+    <div className={styles.user_card} onClick={() => {
+        toggleLogoutButton();
+      }}>
+      <div className={styles.user_info}>
+        <img src={user ? user.photoURL : ""} alt="User" />
+        <div className={styles.user_details}>
+          <p>{user ? user.displayName : "Guest User"}</p>
+          <p>{user ? user.email : ""}</p>
         </div>
-      ) : (
-        <p>Guest User</p>
-      )}
+      </div>
+      {showLogoutButton && <button onClick={handleLogout} className={styles.logout_button}>
+        Logout
+      </button>}
+      
     </div>
+    {showLogoutDialog && (
+        <div className={styles.logout_dialog}>
+          <p>Are you sure you want to log out?</p>
+          <button onClick={confirmLogout}>Yes</button>
+          <button onClick={cancelLogout}>No</button>
+        </div>
+      )}
+      {showLogoutDialog && (
+        <div
+          className={styles.overlay}
+          onClick={cancelLogout} // Close the dialog when the overlay is clicked
+        />
+      )}
+      {showLogoutButton && (
+        <div
+          className={styles.overlay_button}
+          onClick={toggleLogoutButton} // Close the dialog when the overlay is clicked
+        />
+      )}
+    </>
   );
 };
 
 export default UserCard;
+
